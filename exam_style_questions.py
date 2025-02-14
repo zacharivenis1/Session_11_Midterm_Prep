f9aef6670bd8b8a2b075e0c9954d4f1dee7b592e
zacharivenis1 <zach.venis@gmail.com>
2025-02-14 11:38:14 +0100

    several exercises for midterm prep

---
+++ exam_style_questions.py
@@ -0,0 +1,27 @@
+#Additional Notes for Midterm
+#None type is the type of the none value and boolean is for True and False
+
+#the type() function will tell us the type
+
+#6//2 ensures the result is an int instead of a float
+
+print(type(5 or 6))
+
+print(type(5 and 7.2))
+
+print(type(2*2))
+
+print(type(print(2*2))) #its a none type because the print function returns none
+
+print(type("123".upper()))
+
+print(type("abc"[2]))
+
+print(len(["abc",2]))
+
+print([] or "" or 0/10 or 7/2 or 9)
+
+print(9 and 10 and 7/2 and "" and 11/2)
+
+print([1,2,3].append("John"))
+
